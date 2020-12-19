0294315434bf4fc4f83cf729427b6e48f921b2ef
tommieg <tommieg@kth.se>
2020-12-19 13:14:16 +0100

    high-scores

---
+++ high-scores/high_scores.py
@@ -0,0 +1,11 @@
+def latest(scores):
+    return scores.pop()
+
+def personal_best(scores):
+    return max(scores)
+
+def personal_top_three(scores):
+    scores.sort(reverse = True)
+    if len(scores) < 3:
+        return scores
+    else: return scores[0:3]
