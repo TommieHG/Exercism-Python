e3bd08b06ec84f336825bf2096abae78f25112bd
tommieg <tommieg@kth.se>
2020-12-28 16:51:51 +0100

    scrabble score

---
+++ scrabble-score/scrabble_score.py
@@ -0,0 +1,9 @@
+def score(word):
+
+    #setup a dictionary
+    score_table = {"a": 1, "b": 3, "c": 3, "d": 2, "e": 1, "f": 4, "g": 2, "h": 4, "i": 1, "j": 8, "k": 5, "l": 1, "m": 3, "n": 1, "o": 1, "p": 3, "q": 10, "r": 1, "s": 1, "t": 1, "u": 1, "v": 4, "w": 4, "x": 8, "y": 4, "z": 10}
+
+    #look for the value of each letter and sum it up
+    return sum(score_table[i] for i in word.lower())
+    
+    
